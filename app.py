62c6ae656588638fbb595eb0e8730e32c553ce1c
Aayush Jitendra Kumar <jitendra.kumar.epf@gmail.com>
2022-09-30 14:55:40 +0530

    display added

--- app.py
+++ app.py
@@ -1,20 +1,34 @@
 import streamlit as st
-from functions import convert_pdf_to_txt_file, save_pages, convert_pdf_to_txt_pages
-
-
-
-
-
+import base64
+from functions import convert_pdf_to_txt_file, save_pages, convert_pdf_to_txt_pages, displayPDF
+from pathlib import Path
+import tempfile
 with st.sidebar:
     st.title("PDF to Text")
     pdf_file = st.file_uploader("Load your PDF file", type="pdf")
+    # print(pdf_file.name)
+    # number1 = st.number_input('First Page')
+    # number2 = st.number_input('Last Page')
+    # values = st.slider('Select a range of pages',0.0,500.0,(number1,number2))
+# st.write('Values:', values)
+ 
+   
     
 
 
 if pdf_file:
     # display document
+    # print(pdf_file.name)
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        st.markdown("## Original PDF File")
+        fp = Path(tmp_file.name)
+        fp.write_bytes(pdf_file.getvalue())
+        st.write(displayPDF(tmp_file.name))
     # with st.expander("Display document"):
-    #     displayPDF(pdf_file)
+    #     displayPDF(pdf_file.name)
+
+       
+        
     
     # pdf to text
     # if textOutput == 'One text file (.txt)':
@@ -35,4 +49,4 @@ if pdf_file:
     #             file_name="pdf_to_txt.zip",
     #             mime="application/zip"
     #         )
-    
+   
--- functions.py
+++ functions.py
@@ -8,6 +8,8 @@ from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
 import base64
+import tempfile
+from pathlib import Path
 
 
 @st.cache
@@ -92,3 +94,14 @@ def save_pages(pages):
 #     pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 #     # Displaying File
 #     st.markdown(pdf_display, unsafe_allow_html=True)
+
+def displayPDF(file_path:str):
+    # Opening file from file path
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000" type="application/pdf">'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
